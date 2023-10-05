2862f324e09c1d37ab1e56f0b46b2ecaaaa18b90
Abu Afeez <abuafeezabolarinwa@yahoo.com>
2023-10-05 13:31:54 +0100

    add more views

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
 from . import views
 
 
+router = routers.DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
+
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('booking/', include(router.urls)),
+    path('menu/', views.MenuItemView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
